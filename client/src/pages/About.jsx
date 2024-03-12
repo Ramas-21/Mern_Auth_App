23f7604317df0f8966b43b63650b87165c3d4cad
Lawrence <lawrencehome21@gmail.com>
2024-03-12 13:50:52 -0700

    completed home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,14 @@
 const About = () => {
   return (
-    <div>
-      About
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About
+      </h1>
+      <p className="mb-4 text-slate-700"></p>
+      <p className="mb-4 text-slate-700"></p>
+      <p className="mb-4 text-slate-700"></p>
     </div>
-  )
-}
+  );
+};
 
 export default About;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Welcome to my Authentication App</h1>
+      <p className="mb-4 text-slate-700"></p>
+      <p className="mb-4 text-slate-700"></p>
+      <p className="mb-4 text-slate-700"></p>
     </div>
   )
 }
